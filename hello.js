c597867b5a0ba3df5a835a72867a3a53ee5309b0
cse120f <carrickdb@gmail.com>
2017-12-18 18:21:15 -0800

    used for testing enableHashing

--- hello.js
+++ hello.js
@@ -1,4 +1,4 @@
 console.log("Hello, Dave.")
-
+//console.log("whahahahahaha")
 //var stack = new Error().stack
 //console.log( stack )
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-require('./app/index')
+//console.log(Object.keys(process.binding('util')));
+const {enableHashing} = process.binding('util');
+
+enableHashing("/Users/carrickdb/Dropbox/node_proj/test/src_hashes");
+const net = require('net');
+require("/Users/carrickdb/Dropbox/node_proj/test/mal.js");
+require("/Users/carrickdb/Dropbox/node_proj/test/hello.js");
